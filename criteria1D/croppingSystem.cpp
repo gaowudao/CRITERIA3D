0aabc8dddd299be09ca759055851fd2741d7a52a
Fausto Tomei <ftomei@arpae.it>
2017-11-21 16:03:21 +0100

    bug

--- criteria1D/croppingSystem.cpp
+++ criteria1D/croppingSystem.cpp
@@ -290,7 +290,6 @@ float cropIrrigationDemand(Criteria1D* myCase, float currentPrec, float nextPrec
 
     // all check passed --> IRRIGATION
 
-
     // reset irrigation shift
     daysSinceIrrigation = 0;
 
