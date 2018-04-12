83b982c6f975f33c5b4b5e1552fe433ea3de8b8e
laura <laura@laura0.qwerty.local>
2018-04-12 11:17:49 +0200

    updateGridDate fix check error

--- dbMeteoGrid/dbMeteoGrid.cpp
+++ dbMeteoGrid/dbMeteoGrid.cpp
@@ -655,7 +655,7 @@ bool Crit3DMeteoGridDbHandler::updateGridDate(std::string *myError)
             statement = QString("SELECT MIN(PragaTime) as minDate, MAX(PragaTime) as maxDate FROM `%1`").arg(tableD);
             qry.exec(statement);
         }
-        std::cout << "qry.lastError().number() " << qry.lastError().number() << std::endl;
+
         if ( !qry.lastError().type() == QSqlError::NoError && qry.lastError().number() != tableNotFoundError)
         {
             *myError = qry.lastError().text().toStdString();
