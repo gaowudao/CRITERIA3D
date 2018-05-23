0952363219756e3a552e84c823d42c3c477587b6
Fausto Tomei <fausto.tomei@gmail.com>
2018-05-23 18:54:57 +0200

    moses

--- criteria1D/modelCore.h
+++ criteria1D/modelCore.h
@@ -4,6 +4,7 @@
     #include <string>
     class Crit3DDate;
     class Criteria1D;
+    class Criteria1DUnit;
 
     bool runModel(Criteria1D* myCase, Criteria1DUnit *myUnit, std::string* myError);
     bool computeModel(Criteria1D* myCase, const Crit3DDate& firstDate, const Crit3DDate& lastDate, std::string* myError);
