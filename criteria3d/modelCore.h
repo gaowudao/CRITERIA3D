b43208afb6fb1551eed9eba4361a863c372e45ef
Fausto Tomei <fausto.tomei@gmail.com>
2018-06-11 20:19:25 +0200

    clean meteo

--- criteria3d/modelCore.h
+++ criteria3d/modelCore.h
@@ -1,14 +1,9 @@
 #ifndef MODELCORE_H
 #define MODELCORE_H
 
-    #ifndef QSTRING_H
-        #include <QString>
-    #endif
-    #ifndef CRIT3DDATE_H
-        #include "crit3dDate.h"
-    #endif
-
     class Crit3DProject;
+    class Crit3DDate;
+    class QString;
 
     bool modelDailyCycle(bool isInitialState, Crit3DDate myDate, int nrHours, Crit3DProject* myProject,
                          const QString& myOutputPath, bool isSave, const QString& myArea);
