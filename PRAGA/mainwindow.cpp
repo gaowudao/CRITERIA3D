b3ca9b62878b0e745a0660148b8a4ba5fcf68cfb
laura <laura@laura0.qwerty.local>
2018-07-23 11:58:46 +0200

    visualization elab todo

--- PRAGA/mainwindow.cpp
+++ PRAGA/mainwindow.cpp
@@ -1248,13 +1248,38 @@ void MainWindow::on_actionElaboration_meteo_points_triggered()
         }
         else
         {
-            return;
+            Crit3DColor *myColor;
+            for (int i = 0; i < myProject.nrMeteoPoints; i++)
+            {
+                myProject.meteoPoints[i].currentValue = myProject.meteoPoints[i].elaboration;
+                if (myProject.meteoPoints[i].currentValue != NODATA)
+                {
+                    if (myProject.meteoPoints[i].myQuality == quality::accepted)
+                    {
+                        pointList[i]->setRadius(5);
+                        myColor = myProject.meteoPointsColorScale->getColor(myProject.meteoPoints[i].currentValue);
+                        pointList[i]->setFillColor(QColor(myColor->red, myColor->green, myColor->blue));
+                    }
+                    else
+                    {
+                        // Wrong data
+                        pointList[i]->setRadius(15);
+                        pointList[i]->setFillColor(QColor(Qt::black));
+                    }
+
+                    pointList[i]->setToolTip(i);
+                    pointList[i]->setVisible(true);
+                }
+            }
+
+            meteoPointsLegend->update();
         }
     }
     else
     {
          myProject.logError();
     }
+    return;
 
 }
 
