6f581a824cc8f91f6bbd86c6c24d7ab279e7656f
laura <laura@laura0.qwerty.local>
2018-08-03 14:55:02 +0200

    showElab grid

--- PRAGA/project.cpp
+++ PRAGA/project.cpp
@@ -1235,6 +1235,7 @@ bool Project::elaboration(bool isMeteoGrid, bool isAnomaly)
         {
             return false;
         }
+        this->meteoGridDbHandler->meteoGrid()->fillMeteoRasterElabValue();
     }
     else
     {
