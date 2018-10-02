63c4826ed7ba85bb62e23ba50c93473ca3cf7ea8
gantolini <gantolini@arpae.it>
2018-10-02 14:56:25 +0200

    added interpolationCmd

---
+++ PRAGA/shared/interpolationcmd.h
@@ -0,0 +1,22 @@
+#ifndef INTERPOLATIONCMD_H
+#define INTERPOLATIONCMD_H
+
+#ifndef GIS_H
+    #include "gis.h"
+#endif
+#ifndef METEO_H
+    #include "meteo.h"
+#endif
+#ifndef INTERPOLATIONSETTINGS_H
+    #include "interpolationSettings.h"
+#endif
+#ifndef INTERPOLATIONPOINT
+    #include "interpolationPoint.h"
+#endif
+
+bool loadProxyGrid(Crit3DProxy* myProxy);
+
+bool interpolationRaster(std::vector <Crit3DInterpolationDataPoint> &myPoints, Crit3DInterpolationSettings* mySettings,
+                        gis::Crit3DRasterGrid* myGrid, const gis::Crit3DRasterGrid& myDTM, meteoVariable myVar, bool showInfo);
+
+#endif // INTERPOLATIONCMD_H
