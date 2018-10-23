998940f4772c96bfd9e1723149926ae94e73ad20
Fausto Tomei <fausto.tomei@gmail.com>
2018-10-23 15:17:18 +0200

    remove waterbalance3d

--- waterBalance3D/modelCore3D.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODELCORE3D_H
-#define MODELCORE3D_H
-
-    class Crit3DProject;
-    class Crit3DTime;
-
-    bool runModel(Crit3DProject* myProject, Crit3DTime myCurrentTime, bool isInitialState);
-
-#endif // MODELCORE3D_H
