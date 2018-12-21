bf1a7774e880d8f05d9b953678f4ddc6ec179b05
gantolini <gantolini@arpae.it>
2018-12-21 10:49:04 +0100

    diffuse clear sky irradiance rewritten

--- solarRadiation/solarRadiation.cpp
+++ solarRadiation/solarRadiation.cpp
@@ -344,6 +344,31 @@ namespace radiation
         return mySunPosition->extraIrradianceNormal * getSinDecimalDegree(mySunPosition->elevation) * (float)exp(-0.8662 * myLinke * airMass * rayleighThickness);
     }
 
+    /*!
+     * \brief Diffuse irradiance on a horizontal surface [W m-2]
+     * \brief (Rigollier et al. 2000)
+     * \param myLinke
+     * \param mySunPosition a pointer to a TsunPosition
+     * \return result
+     */
+    float clearSkyDiffuseHorizontal(float myLinke, TsunPosition* mySunPosition)
+    {
+        double Fd; /*!< diffuse solar altitude function [] */
+        double Trd;         /*!< transmission function                                           []   */
+        double A0, A1, A2;
+        double sinElev;
+
+        Trd = -0.015843 + myLinke * (0.030543 + 0.0003797 * myLinke);
+
+        sinElev = maxValue(getSinDecimalDegree(mySunPosition->elevation), 0.);
+        A0 = 0.26463 + myLinke * (-0.061581 + 0.0031408 * myLinke);
+        if ((A0 * Trd) < 0.0022) A0 = 0.002 / Trd;
+        A1 = 2.0402 + myLinke * (0.018945 - 0.011161 * myLinke);
+        A2 = -1.3025 + myLinke * (0.039231 + 0.0085079 * myLinke);
+        Fd = A0 + A1 * sinElev + A2 * sinElev * sinElev;
+
+        return float(mySunPosition->extraIrradianceNormal * Fd * Trd);
+    }
 
     /*!
      * \brief Beam irradiance on an inclined surface                         [W m-2]
@@ -356,36 +381,6 @@ namespace radiation
         /*! Bh: clear sky beam irradiance on a horizontal surface */
         return (beamIrradianceHor * getSinDecimalDegree(mySunPosition->incidence) / getSinDecimalDegree(mySunPosition->elevationRefr)) ;
     }
-
-    /*!
-     * \brief Diffuse irradiance on a horizontal surface (Scharmer and Greif, 2000   [W m-2]
-     * \param myLinke
-     * \param mySunPosition a pointer to a TsunPosition
-     * \return result
-     */
-    float clearSkyDiffuseHorizontal(float myLinke, TsunPosition* mySunPosition)
-    {
-        double diffuseSolarAltitudeFunction; /*!< diffuse solar altitude function (Scharmer and Greif, 2000)     []   */
-        double transmissionFunction;         /*!< ransmission function                                           []   */
-        double A1b, a1, a2, A3;
-        double sinElev;
-
-        sinElev = maxValue(getSinDecimalDegree(mySunPosition->elevation), float(0.00001));
-        transmissionFunction = -0.015843 + myLinke * (0.030543 + 0.0003797 * myLinke);
-        A1b = 0.26463 + myLinke * (-0.061581 + 0.0031408 * myLinke);
-
-        if ((A1b * transmissionFunction) < 0.0022)
-            a1 = 0.0022 / transmissionFunction;
-        else
-            a1 = A1b;
-
-        a2 = 2.0402 + myLinke * (0.018945 - 0.011161 * myLinke);
-        A3 = -1.3025 + myLinke * (0.039231 + 0.0085079 * myLinke);
-        diffuseSolarAltitudeFunction = a1 + a2 * sinElev + A3 * sinElev * sinElev;
-
-        return float(mySunPosition->extraIrradianceNormal * diffuseSolarAltitudeFunction * transmissionFunction);
-    }
-
 
     /*!
      * \brief Diffuse irradiance on an inclined surface (Muneer, 1990)               [W m-2]
