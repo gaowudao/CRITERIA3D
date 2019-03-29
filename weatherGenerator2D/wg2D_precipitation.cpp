4ffe81a79809545d8797941ae38b9a42d1e29bbf
voltAntonio <avolta@arpa.emr.it>
2019-03-29 13:15:26 +0100

    aggiunta files

---
+++ weatherGenerator2D/wg2D_precipitation.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <time.h>
+#include <iostream>
+
+#include "wg2D.h"
+#include "commonConstants.h"
+#include "furtherMathFunctions.h"
+#include "statistics.h"
+#include "eispack.h"
+#include "gammaFunction.h"
