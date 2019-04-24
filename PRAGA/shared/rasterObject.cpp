5598d15363d1b2cdc4f41c6cc5752901b16c55f7
Fausto Tomei <ftomei@arpae.it>
2019-04-24 15:35:09 +0200

    info

--- PRAGA/shared/rasterObject.cpp
+++ PRAGA/shared/rasterObject.cpp
@@ -1,3 +1,31 @@
+/*!
+    \file rasterObject.cpp
+
+    \abstract draw raster in MapGraphics widget
+
+    This file is part of CRITERIA-3D distribution.
+
+    CRITERIA-3D has been developed by A.R.P.A.E. Emilia-Romagna.
+
+    \copyright
+    CRITERIA-3D is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    CRITERIA-3D is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with CRITERIA-3D.  If not, see <http://www.gnu.org/licenses/>.
+
+    \authors
+    Fausto Tomei ftomei@arpae.it
+    Gabriele Antolini gantolini@arpae.it
+    Laura Costantini laura.costantini0@gmail.com
+*/
+
+
 #include "commonConstants.h"
 #include "rasterObject.h"
 
