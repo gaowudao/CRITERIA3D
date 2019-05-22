1958913ef82f5f50f2bc25b8ef0e9add7a4023f4
laura <laura@laura0.qwerty.local>
2019-05-22 14:19:43 +0200

    check utm and proj

--- PRAGA_GIS/unitCropMap.cpp
+++ PRAGA_GIS/unitCropMap.cpp
@@ -3,12 +3,22 @@
 bool zonalStatistics(Crit3DShapeHandler* shapeRef, Crit3DShapeHandler* shapeVal, std::string valField, DBFFieldType fieldType, int cellSize, opType type)
 {
 
-    // check shape
+    // check shape type
     if ( shapeRef->getTypeString() != shapeVal->getTypeString() || shapeRef->getTypeString() != "2D Polygon" )
     {
         return false;
     }
     // check proj
+    if (shapeRef->getIsWGS84() == false || shapeVal->getIsWGS84() == false)
+    {
+        return false;
+    }
+    // check utm zone
+    if (shapeRef->getUtmZone() != shapeVal->getUtmZone())
+    {
+        return false;
+    }
+
     return true;
 
 
