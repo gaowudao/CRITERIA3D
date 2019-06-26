39bf14b87eb347f1da14f6d3311e625cf42ba5fd
voltAntonio <avolta@arpae.it>
2019-06-26 18:27:46 +0200

    bla bla

--- specialMathFunctions/furtherMathFunctions.h
+++ specialMathFunctions/furtherMathFunctions.h
@@ -52,6 +52,7 @@
 
     namespace interpolation
     {
+        double linearInterpolation (double x, double *xColumn , double *yColumn, int dimTable);
         float linearInterpolation (float x, float *xColumn , float *yColumn, int dimTable );
         float linearExtrapolation(double x3,double x1,double y1,double x2 , double y2);
         double normGeneric(double *myParameters, int nrMyParameters, double *myX,int nrMyX, double *myY,int myFunctionCode);
