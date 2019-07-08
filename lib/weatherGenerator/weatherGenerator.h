0171cdef4ce2157a5a26790262ef7fad991567be
Fausto Tomei <ftomei@arpae.it>
2019-07-08 19:40:11 +0200

    wg improvement

--- lib/weatherGenerator/weatherGenerator.h
+++ lib/weatherGenerator/weatherGenerator.h
@@ -21,37 +21,37 @@
 
     struct TmonthlyWheather
     {
-        float monthlyTmin [12];           //[°C] monthly maximum temp.
-        float monthlyTmax [12];           //[°C] monthly minimum temp.
-        float sumPrec [12];               //[mm] total monthly precipitation
-        float fractionWetDays [12];       //[-] fraction of wet days (must be >0)
-        float dw_Tmax [12];               //[°C]difference between maximum temperatures on dry and wet days
-        float probabilityWetWet[12];      //[-] probability of a wet day after a wet day
-        float stDevTmin [12];             //[-] monthly minimum temperature standard deviation
-        float stDevTmax [12];             //[-] monthly maximum temperature standard deviation
+        float monthlyTmin [12];           // [°C]   monthly maximum temp.
+        float monthlyTmax [12];           // [°C]   monthly minimum temp.
+        float sumPrec [12];               // [mm]   total monthly precipitation
+        float fractionWetDays [12];       // [-]    fraction of wet days (must be >0)
+        float dw_Tmax [12];               // [°C]   difference between maximum temperatures on dry and wet days
+        float probabilityWetWet[12];      // [-]    probability of a wet day after a wet day
+        float stDevTmin [12];             // [-]    monthly minimum temperature standard deviation
+        float stDevTmax [12];             // [-]    monthly maximum temperature standard deviation
     };
 
     struct TdailyWheather
     {
-        float pww [366];                  //[-] daily probability wet/wet
-        float pwd [366];                  //[-] daily probability wet/dry
-        float meanPrecip [366];           //[mm] average mm/wet day
-        float meanDryTMax [366];          //[°C] daily maximum temperature on dry days
-        float meanWetTMax [366];          //[°C] daily maximum temperature on wet days
-        float meanTMin [366];             //[°C] daily minimum temperature
-        float maxTempStd [366];           //[°C] daily maximum temperature standard deviation
-        float minTempStd [366];           //[°C] daily minimum temperature standard deviation
+        float pww [366];                  // [-]    daily probability wet/wet
+        float pwd [366];                  // [-]    daily probability wet/dry
+        float meanPrecip [366];           // [mm]   average mm/wet day
+        float meanDryTMax [366];          // [°C]   daily maximum temperature on dry days
+        float meanWetTMax [366];          // [°C]   daily maximum temperature on wet days
+        float meanTMin [366];             // [°C]   daily minimum temperature
+        float maxTempStd [366];           // [°C]   daily maximum temperature standard deviation
+        float minTempStd [366];           // [°C]   daily minimum temperature standard deviation
     };
 
     struct TstateWheather
     {
-        int currentDay;                     // [-] day we have just passed as argument to "newday" function
-        float currentTmax;                  // [°C] maximum temperature of the day passed as argument to "newday" function
-        float currentTmin;                  // [°C] minimum temperature of the day passed as argument to "newday" function
-        float currentPrec;                  // [mm] precipitation of the day passed as argument to "newday" function
-        float resTMaxPrev;                  // [-] residual of maximum temperature for previous day
-        float resTMinPrev;                  // [-] residual of minimum temperature for previous day
-        bool wetPreviousDay;                // [-] true if the previous day has been a wet day, false otherwise
+        int currentDay;                    // [-]   day we have just passed as argument to "newday" function
+        float currentTmax;                 // [°C]  maximum temperature of the day passed as argument to "newday" function
+        float currentTmin;                 // [°C]  minimum temperature of the day passed as argument to "newday" function
+        float currentPrec;                 // [mm]  precipitation of the day passed as argument to "newday" function
+        float resTMaxPrev;                 // [-]   residual of maximum temperature for previous day
+        float resTMinPrev;                 // [-]   residual of minimum temperature for previous day
+        bool wetPreviousDay;               // [-]   true if the previous day has been a wet day, false otherwise
     };
 
     struct TwheatherGenClimate
