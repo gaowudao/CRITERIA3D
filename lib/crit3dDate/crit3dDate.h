45911c5afa9f71906015d81efc55f5315746e15c
Fausto Tomei <ftomei@arpae.it>
2019-07-08 19:43:22 +0200

    wg improvement

--- lib/crit3dDate/crit3dDate.h
+++ lib/crit3dDate/crit3dDate.h
@@ -1,33 +1,3 @@
-/*!
-    CRITERIA3D
-
-    \copyright 2016 Fausto Tomei, Gabriele Antolini,
-    Alberto Pistocchi, Marco Bittelli, Antonio Volta, Laura Costantini
-
-    You should have received a copy of the GNU General Public License
-    along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
-
-    This file is part of CRITERIA3D.
-    CRITERIA3D has been developed under contract issued by A.R.P.A. Emilia-Romagna
-
-    CRITERIA3D is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    CRITERIA3D is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with CRITERIA3D.  If not, see <http://www.gnu.org/licenses/>.
-
-    contacts:
-    fausto.tomei@gmail.com
-    ftomei@arpae.it
-*/
- 
 #ifndef CRIT3DDATE_H
 #define CRIT3DDATE_H
 
