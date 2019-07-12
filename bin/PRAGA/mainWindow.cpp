7f411aaa034ea7ebb4b941abde956d474dfcf668
laura <laura@laura0.qwerty.local>
2019-07-12 16:50:07 +0200

    TestSoilWidget.pro

--- bin/PRAGA/mainWindow.cpp
+++ bin/PRAGA/mainWindow.cpp
@@ -833,8 +833,8 @@ void MainWindow::redrawMeteoGrid(visualizationType showType)
     case showLocation:
     {
         this->ui->actionShowGridLocation->setChecked(true);
-        meteoGridObj->setDrawBorders(true);
         myProject.meteoGridDbHandler->meteoGrid()->fillMeteoRasterNoData();
+        meteoGridObj->setDrawBorders(true);
         break;
     }
     case showCurrentVariable:
