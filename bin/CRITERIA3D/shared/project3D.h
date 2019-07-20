a1030fbeffd13afe339384f742465b0919e34439
Fausto Tomei <ftomei@arpae.it>
2019-07-20 17:40:40 +0200

    project3D

---
+++ bin/CRITERIA3D/shared/project3D.h
@@ -0,0 +1,28 @@
+#ifndef PROJECT3D_H
+#define PROJECT3D_H
+
+    #ifndef PROJECT_H
+        #include "project.h"
+    #endif
+    #ifndef METEOMAPS_H
+        #include "meteoMaps.h"
+    #endif
+    #ifndef SOIL3D_H
+        #include "soil3D.h"
+    #endif
+
+    class Project3D : public Project
+    {
+
+    public:
+        Crit3DMeteoMaps* meteoMaps;
+        gis::Crit3DRasterGrid soilMap;
+        Crit3DSoilFluxesSettings* WBSettings;
+
+        Project3D();
+
+
+    };
+
+
+#endif // PROJECT3D_H
