0a60df22b00a68688104b9f5db005e8b3e72a089
laura <laura@laura0.qwerty.local>
2019-07-25 17:13:33 +0200

    accept empty values as input

--- lib/soilWidget/tableDelegate.cpp
+++ lib/soilWidget/tableDelegate.cpp
@@ -9,9 +9,10 @@ TableDelegate::TableDelegate(QObject *parent) : QStyledItemDelegate(parent)
 QWidget* TableDelegate::createEditor(QWidget* parent,const QStyleOptionViewItem &option,const QModelIndex &index) const
 {
     QLineEdit* editor = new QLineEdit(parent);
-    QDoubleValidator* val = new QDoubleValidator(editor);
-    val->setBottom(0);
-    val->setNotation(QDoubleValidator::StandardNotation);
+    double bottom = 0;
+    double top = 90000;
+    int d = 5;
+    myValidator* val = new myValidator(bottom, top, d, editor);
     editor->setValidator(val);
     return editor;
 }
@@ -27,10 +28,63 @@ void TableDelegate::setModelData(QWidget* editor,QAbstractItemModel* model,const
 {
     QLineEdit* line = static_cast<QLineEdit*>(editor);
     QString value = line->text();
-    model->setData(index,value);
+
+    if (value.isEmpty())
+    {
+        model->setData(index, EMPTYVALUE);
+    }
+    else
+    {
+        model->setData(index,value);
+    }
+
 }
 
 void TableDelegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     editor->setGeometry(option.rect);
+}
+
+// myValidator Constructor
+myValidator::myValidator ( double bottom, double top, int decimals,
+        QObject* parent = 0 )
+    : QDoubleValidator ( bottom, top, decimals, parent ) {
+
+}
+
+// Custom validate function to allow empty values
+QValidator::State myValidator::validate ( QString& input, int& ) const {
+
+    const double b = bottom();
+    const double t = top();
+    const int d = decimals();
+
+    // Check if the input is empty
+    if ( input.isEmpty() ) {
+        return Acceptable;
+    }
+    else
+    {
+        bool ok;
+        double entered = input.toDouble ( &ok );
+        if ( !ok && !input.size() ) {
+            // Handle an edge case where there are no characters left.
+            ok = true;
+        } else if ( !ok && ( input == "." ) ) {
+            // If the only character is a decimal, then it is fine.
+            ok = true;
+        }
+
+        if ( !ok || entered > t || entered < b ) {
+            // Not a number or out of range, so invalid
+            return Invalid;
+        } else if ( input.contains ( "." ) ) { // If it cant, check for a decimal point and how many numbers follow
+            QStringList dec = input.split ( "." );
+            if ( dec[1].size() > d ) {
+                // Too many decimals
+                return Invalid;
+            }
+        }
+    }
+    return Acceptable;
 }
--- lib/soilWidget/tableDelegate.h
+++ lib/soilWidget/tableDelegate.h
@@ -4,6 +4,8 @@
 #include <QStyledItemDelegate>
 // class to have finer control over the editing of items
 
+#define EMPTYVALUE -9999  //string for empty input
+
 class TableDelegate : public QStyledItemDelegate
 {
 Q_OBJECT
@@ -14,6 +16,14 @@ public:
     void setEditorData(QWidget *editor, const QModelIndex &index) const;
     void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
     void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+};
+
+// custom QDoubleValidator to allow empty values
+class myValidator : public QDoubleValidator {
+
+public:
+    myValidator ( double bottom, double top, int decimals, QObject* parent );
+    QValidator::State validate ( QString& input, int& ) const;
 };
 
 
