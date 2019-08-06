3be4275e01e70ccbd1fc0a4992700380fc217dc1
gantolini <gantolini@arpae.it>
2019-08-06 11:09:14 +0200

    praga fixed

--- bin/PRAGA/mainWindow.cpp
+++ bin/PRAGA/mainWindow.cpp
@@ -848,7 +848,7 @@ void MainWindow::redrawMeteoPoints(visualizationType showType, bool updateColorS
         // quality control
         checkData(myProject.quality, myProject.getCurrentVariable(),
                   myProject.meteoPoints, myProject.nrMeteoPoints, myProject.getCurrentTime(),
-                  &myProject.qualityInterpolationSettings, myProject.checkSpatialQuality);
+                  &myProject.qualityInterpolationSettings, &(myProject.climateParameters), myProject.checkSpatialQuality);
 
         if (updateColorSCale)
         {
