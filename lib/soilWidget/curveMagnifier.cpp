43d0188f9aa9acd8a8878dcc0634376fd665114d
laura <laura@laura0.qwerty.local>
2019-08-20 15:25:28 +0200

    curvePanner moveCanvas

--- lib/soilWidget/curveMagnifier.cpp
+++ lib/soilWidget/curveMagnifier.cpp
@@ -3,6 +3,9 @@
 #include "qwt_plot.h"
 #include "qwt_scale_div.h"
 
+#define YMAXRANGE 2
+#define YMINRANGE 0.3
+#define YMINLOWERBOUND -0.5
 
 Crit3DCurveMagnifier::Crit3DCurveMagnifier(QwtPlot *plot, int axisIndex) : QwtPlotMagnifier(plot->canvas()), qwtPlot(plot), index(axisIndex)
 {
@@ -29,15 +32,15 @@ void Crit3DCurveMagnifier::rescale(double factor)
     //y
     if (index == QwtPlot::yLeft)
     {
-        if (scaleDiv->range() > 2 || scaleDiv->lowerBound() < -0.5)
+        if (scaleDiv->range() > YMAXRANGE || scaleDiv->lowerBound() < YMINLOWERBOUND)
         {
-            width_2 = 2 / 2 * factor;
+            width_2 = YMAXRANGE / 2 * factor;
             center = width_2 / 2;
             qwtPlot->setAxisScale(index, center - width_2, center + width_2);
         }
-        else if(scaleDiv->range() < 0.3)
+        else if(scaleDiv->range() < YMINRANGE)
         {
-            width_2 = 0.3 / 2 * factor;
+            width_2 = YMINRANGE / 2 * factor;
             center = width_2 / 2;
             qwtPlot->setAxisScale(index, center - width_2, center + width_2);
         }
--- lib/soilWidget/curvePanner.cpp
+++ lib/soilWidget/curvePanner.cpp
@@ -1,6 +1,12 @@
 #include "curvePanner.h"
 #include "qwt_scale_div.h"
 #include "qwt_plot.h"
+#include <math.h>
+
+#define DXMIN 0.0001
+#define DXMAX 10000000
+#define DYMIN -0.2
+#define DYMAX 1.2
 
 Crit3DCurvePanner::Crit3DCurvePanner(QwtPlot *plot) : QwtPlotPanner (plot->canvas()), qwtPlot(plot)
 {
@@ -20,9 +26,6 @@ void Crit3DCurvePanner::moveCanvas(int dx, int dy)
 
     for ( int axis = 0; axis < QwtPlot::axisCnt; axis++ )
     {
-//        if ( !d_data->isAxisEnabled[axis] )
-//            continue;
-
         const QwtScaleMap map = qwtPlot->canvasMap(axis);
 
         const QwtScaleDiv *scaleDiv;
@@ -41,8 +44,35 @@ void Crit3DCurvePanner::moveCanvas(int dx, int dy)
             d1 = map.invTransform(i1 - dy);
             d2 = map.invTransform(i2 - dy);
         }
-        qDebug() << "d1 " << d1;
-        qDebug() << "d2 " << d2;
+        double range;
+        if (axis == QwtPlot::xBottom)
+        {
+            range = log10(d2) - log10(d1);
+            if(d1 < DXMIN)
+            {
+                d1 = DXMIN;
+                d2 = pow(10, (range + log10(d1)));
+            }
+            if(d2 > DXMAX)
+            {
+                d2 = DXMAX;
+                d1 = pow(10, (log10(d2) - range));
+            }
+        }
+        if (axis == QwtPlot::yLeft)
+        {
+            double range = d2 - d1;
+            if(d1 < DYMIN)
+            {
+                d1 = DYMIN;
+                d2 = range + d1;
+            }
+            if(d2 > DYMAX)
+            {
+                d2 = DYMAX;
+                d1 = d2 - range;
+            }
+        }
         qwtPlot->setAxisScale(axis, d1, d2);
     }
 
