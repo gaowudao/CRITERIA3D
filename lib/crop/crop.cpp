80fe9702a873f77480bac663c22cd71806aa7e68
ftomei <ftomei@arpae.it>
2019-09-25 19:11:07 +0200

    clean crop library

--- lib/crop/crop.cpp
+++ lib/crop/crop.cpp
@@ -39,216 +39,212 @@
 #include "root.h"
 
 
-    Crit3DCrop::Crit3DCrop()
+Crit3DCrop::Crit3DCrop()
+{
+    idCrop = "";
+    type = HERBACEOUS_ANNUAL;
+
+    /*!
+     * \brief crop cycle
+     */
+    isLiving = false;
+    isEmerged = false;
+    sowingDoy = NODATA;
+    currentSowingDoy = NODATA;
+    doyStartSenescence = NODATA;
+    plantCycle = NODATA;
+    LAImin = NODATA;
+    LAImax = NODATA;
+    LAIgrass = NODATA;
+    LAIcurve_a = NODATA;
+    LAIcurve_b = NODATA;
+    LAIstartSenescence = NODATA;
+    thermalThreshold = NODATA;
+    upperThermalThreshold = NODATA;
+    degreeDaysIncrease = NODATA;
+    degreeDaysDecrease = NODATA;
+    degreeDaysEmergence = NODATA;
+
+    /*!
+     * \brief water need
+     */
+    kcMax  = NODATA;
+    psiLeaf = NODATA;
+    stressTolerance = NODATA;
+    fRAW = NODATA;
+
+    /*!
+     * \brief irrigation
+     */
+    irrigationShift = NODATA;
+    irrigationVolume = NODATA;
+    degreeDaysStartIrrigation = NODATA;
+    degreeDaysEndIrrigation = NODATA;
+    doyStartIrrigation = NODATA;
+    doyEndIrrigation = NODATA;
+    maxSurfacePuddle = NODATA;
+
+    degreeDays = NODATA;
+    LAI = NODATA;
+}
+
+
+bool Crit3DCrop::isWaterSurplusResistant()
+{
+    return (idCrop == "RICE" || idCrop == "KIWIFRUIT" || type == GRASS || type == FALLOW);
+}
+
+
+int Crit3DCrop::getDaysFromTypicalSowing(int myDoy)
+{
+    return (myDoy - sowingDoy) % 365;
+}
+
+
+int Crit3DCrop::getDaysFromCurrentSowing(int myDoy)
+{
+    if (currentSowingDoy != NODATA)
+        return (myDoy - currentSowingDoy) % 365;
+    else
+        return getDaysFromTypicalSowing(myDoy);
+}
+
+
+bool Crit3DCrop::isInsideTypicalCycle(int myDoy)
+{
+    return ((myDoy >= sowingDoy) && (getDaysFromTypicalSowing(myDoy) < plantCycle));
+}
+
+
+bool Crit3DCrop::isPluriannual()
+{
+    return (type == HERBACEOUS_PERENNIAL ||
+            type == GRASS ||
+            type == FALLOW ||
+            type == FRUIT_TREE);
+}
+
+
+bool Crit3DCrop::needReset(Crit3DDate myDate, float latitude, float waterTableDepth)
+{
+    int currentDoy = getDoyFromDate(myDate);
+
+    if (isPluriannual())
     {
-        idCrop = "";
-        type = HERBACEOUS_ANNUAL;
-
-        /*!
-         * \brief crop cycle
-         */
-        isLiving = false;
-        isEmerged = false;
-        sowingDoy = NODATA;
-        currentSowingDoy = NODATA;
-        doyStartSenescence = NODATA;
-        plantCycle = NODATA;
-        LAImin = NODATA;
-        LAImax = NODATA;
-        LAIgrass = NODATA;
-        LAIcurve_a = NODATA;
-        LAIcurve_b = NODATA;
-        LAIstartSenescence = NODATA;
-        thermalThreshold = NODATA;
-        upperThermalThreshold = NODATA;
-        degreeDaysIncrease = NODATA;
-        degreeDaysDecrease = NODATA;
-        degreeDaysEmergence = NODATA;
-
-        /*!
-         * \brief water need
-         */
-        kcMax  = NODATA;
-        psiLeaf = NODATA;
-        stressTolerance = NODATA;
-        fRAW = NODATA;
-
-        /*!
-         * \brief irrigation
-         */
-        irrigationShift = NODATA;
-        irrigationVolume = NODATA;
-        degreeDaysStartIrrigation = NODATA;
-        degreeDaysEndIrrigation = NODATA;
-        doyStartIrrigation = NODATA;
-        doyEndIrrigation = NODATA;
-        maxSurfacePuddle = NODATA;
-
-        degreeDays = NODATA;
-        LAI = NODATA;
-    }
-
-
-    bool Crit3DCrop::isWaterSurplusResistant()
-    {
-        return (idCrop == "RICE" || idCrop == "KIWIFRUIT" || type == GRASS || type == FALLOW);
-    }
-
-
-    int Crit3DCrop::getDaysFromTypicalSowing(int myDoy)
-    {
-        return (myDoy - sowingDoy) % 365;
-    }
-
-
-    int Crit3DCrop::getDaysFromCurrentSowing(int myDoy)
-    {
-        if (currentSowingDoy != NODATA)
-            return (myDoy - currentSowingDoy) % 365;
-        else
-            return getDaysFromTypicalSowing(myDoy);
-    }
-
-
-    bool Crit3DCrop::isInsideTypicalCycle(int myDoy)
-    {
-        return ((myDoy >= sowingDoy) && (getDaysFromTypicalSowing(myDoy) < plantCycle));
-    }
-
-
-    bool Crit3DCrop::isPluriannual()
-    {
-        return (type == HERBACEOUS_PERENNIAL ||
-                type == GRASS ||
-                type == FALLOW ||
-                type == FRUIT_TREE);
-    }
-
-
-    bool Crit3DCrop::needReset(Crit3DDate myDate, float latitude, float waterTableDepth)
-    {
-        int currentDoy = getDoyFromDate(myDate);
-
-        if (isPluriannual())
+        // pluriannual crop: reset at the end of year (january at north / july at south)
+        if ((latitude >= 0 && myDate.month == 1 && myDate.day == 1)
+            || (latitude < 0 && myDate.month == 7 && myDate.day == 1))
         {
-            // pluriannual crop: reset at the end of year (january at north / july at south)
-            if ((latitude >= 0 && myDate.month == 1 && myDate.day == 1)
-                || (latitude < 0 && myDate.month == 7 && myDate.day == 1))
+            isLiving = true;
+            return true;
+        }
+    }
+    else
+    {
+        // annual crop
+        if (isLiving)
+        {
+            // living crop: check end of crop cycle
+            double cycleDD = degreeDaysEmergence + degreeDaysIncrease + degreeDaysDecrease;
+
+            if ((degreeDays > cycleDD) || (getDaysFromCurrentSowing(currentDoy) > plantCycle))
             {
-                isLiving = true;
+                isLiving = false;
                 return true;
             }
         }
         else
         {
-            // annual crop
-            if (isLiving)
-            {
-                // living crop: check end of crop cycle
-                double cycleDD = degreeDaysEmergence + degreeDaysIncrease + degreeDaysDecrease;
+            // naked soil: check sowing
+            int sowingDoyPeriod = 30;
+            float waterTableThreshold = 0.2f;
+            int daysFromSowing = getDaysFromTypicalSowing(currentDoy);
 
-                if ((degreeDays > cycleDD) || (getDaysFromCurrentSowing(currentDoy) > plantCycle))
+            // is sowing possible? (check period and watertable depth)
+            if (daysFromSowing >= 0 && daysFromSowing <= sowingDoyPeriod)
+            {
+                if (isWaterSurplusResistant()
+                        || int(waterTableDepth) == int(NODATA)
+                        || waterTableDepth >= waterTableThreshold)
                 {
-                    isLiving = false;
+                    isLiving = true;
+                    // update sowing doy
+                    currentSowingDoy = sowingDoy + daysFromSowing;
                     return true;
                 }
             }
-            else
-            {
-                // naked soil: check sowing
-                int sowingDoyPeriod = 30;
-                float waterTableThreshold = 0.2f;
-                int daysFromSowing = getDaysFromTypicalSowing(currentDoy);
-
-                // is sowing possible? (check period and watertable depth)
-                if (daysFromSowing >= 0 && daysFromSowing <= sowingDoyPeriod)
-                {
-                    if (isWaterSurplusResistant()
-                            || int(waterTableDepth) == int(NODATA)
-                            || waterTableDepth >= waterTableThreshold)
-                    {
-                        isLiving = true;
-                        // update sowing doy
-                        currentSowingDoy = sowingDoy + daysFromSowing;
-                        return true;
-                    }
-                }
-            }
         }
-
-        return false;
     }
 
+    return false;
+}
 
-    // reset of (already initialized) crop
-    // TODO: partenza intelligente (usando sowing doy e ciclo)
-    void Crit3DCrop::resetCrop(int nrLayers)
+
+// reset of (already initialized) crop
+// TODO: partenza intelligente (usando sowing doy e ciclo)
+void Crit3DCrop::resetCrop(int nrLayers)
+{
+    // roots
+    if (! isPluriannual())
     {
+        roots.rootDensity[0] = 0.0;
+        for (int i = 1; i < nrLayers; i++)
+            roots.rootDensity[i] = 0;
+    }
+
+    isEmerged = false;
+
+    if (isLiving)
+    {
+        degreeDays = 0;
+
+        // LAI
+        LAI = LAImin;
+
+        if (type == FRUIT_TREE)
+            LAI += LAIgrass;
+    }
+    else
+    {
+        degreeDays = NODATA;
+        LAI = NODATA;
+        currentSowingDoy = NODATA;
+
         // roots
-        if (! isPluriannual())
-        {
-            roots.rootDensity[0] = 0.0;
-            for (int i = 1; i < nrLayers; i++)
-                roots.rootDensity[i] = 0;
-        }
-
-        isEmerged = false;
-
-        if (isLiving)
-        {
-            degreeDays = 0;
-
-            // LAI
-            LAI = LAImin;
-
-            if (type == FRUIT_TREE)
-                LAI += LAIgrass;
-        }
-        else
-        {
-            degreeDays = NODATA;
-            LAI = NODATA;
-            currentSowingDoy = NODATA;
-
-            // roots
-            roots.rootLength = 0.0;
-            roots.rootDepth = NODATA;
-        }
-
-        LAIstartSenescence = NODATA;
-        daysSinceIrrigation = NODATA;
+        roots.rootLength = 0.0;
+        roots.rootDepth = NODATA;
     }
 
-
-    speciesType getCropType(std::string cropType)
-    {
-        // lower case
-        std::transform(cropType.begin(), cropType.end(), cropType.begin(), ::tolower);
-
-        if (cropType == "herbaceous")
-            return HERBACEOUS_ANNUAL;
-        else if (cropType == "herbaceous_perennial")
-            return HERBACEOUS_PERENNIAL;
-        else if (cropType == "horticultural")
-            return HORTICULTURAL;
-        else if (cropType == "grass")
-            return GRASS;
-        else if (cropType == "grass_first_year")
-            return GRASS;
-        else if (cropType == "fallow")
-            return FALLOW;
-        else if (cropType == "tree" || cropType == "fruit_tree")
-            return FRUIT_TREE;
-        else
-            return HERBACEOUS_ANNUAL;
-    }
-
-    double computeDegreeDays(double myTmin, double myTmax, double myLowerThreshold, double myUpperThreshold)
-    {
-        return MAXVALUE((myTmin + MINVALUE(myTmax, myUpperThreshold)) / 2. - myLowerThreshold, 0);
-    }
+    LAIstartSenescence = NODATA;
+    daysSinceIrrigation = NODATA;
+}
 
 
+speciesType getCropType(std::string cropType)
+{
+    // lower case
+    std::transform(cropType.begin(), cropType.end(), cropType.begin(), ::tolower);
 
+    if (cropType == "herbaceous")
+        return HERBACEOUS_ANNUAL;
+    else if (cropType == "herbaceous_perennial")
+        return HERBACEOUS_PERENNIAL;
+    else if (cropType == "horticultural")
+        return HORTICULTURAL;
+    else if (cropType == "grass")
+        return GRASS;
+    else if (cropType == "grass_first_year")
+        return GRASS;
+    else if (cropType == "fallow")
+        return FALLOW;
+    else if (cropType == "tree" || cropType == "fruit_tree")
+        return FRUIT_TREE;
+    else
+        return HERBACEOUS_ANNUAL;
+}
 
+double computeDegreeDays(double myTmin, double myTmax, double myLowerThreshold, double myUpperThreshold)
+{
+    return MAXVALUE((myTmin + MINVALUE(myTmax, myUpperThreshold)) / 2. - myLowerThreshold, 0);
+}
 
