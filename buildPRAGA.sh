753b5eddf7e555edfdaa5b7d1ec9a205dd0421a7
lauracosta <lauramail@iol.it>
2019-12-03 22:50:02 +0100

    workaround delete QPalette::PlaceholderText

--- buildPRAGA.sh
+++ buildPRAGA.sh
@@ -2,4 +2,6 @@
 cd bin/Makeall_PRAGA
 /usr/lib/x86_64-linux-gnu/qt5/bin/qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler
 make -f Makefile qmake_all
+# remove QPalette::PlaceholderText because it is not supported, has been added with Qt 5.12
+grep -v 'QPalette::PlaceholderText' ../PRAGA/ui_mainWindow.h > ../PRAGA/ui.h; mv -f ../PRAGA/ui.h ../PRAGA/ui_mainWindow.h
 make
