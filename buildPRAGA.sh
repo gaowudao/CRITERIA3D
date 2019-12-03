8e707f53ae0ef6d599fbe0854ddf4fb33afb8e45
lauracosta <lauramail@iol.it>
2019-12-03 23:36:43 +0100

    travis test with mapgraphics

--- buildPRAGA.sh
+++ buildPRAGA.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+# build mapGraphics
+cd mapGraphics
+qmake MapGraphics.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler
+make
+
+cd ..
+
 cd bin/Makeall_PRAGA
-/opt/qt512/bin/qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler
+qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler
 make -f Makefile qmake_all
 # remove QPalette::PlaceholderText because it is not supported, has been added with Qt 5.12
 # grep -v 'QPalette::PlaceholderText' ../PRAGA/ui_mainWindow.h > ../PRAGA/ui.h; mv -f ../PRAGA/ui.h ../PRAGA/ui_mainWindow.h
