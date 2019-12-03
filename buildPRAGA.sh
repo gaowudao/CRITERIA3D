54ca00ec8a55c8b91ed9218b7ab0b5182e4f23e9
lauracosta <lauramail@iol.it>
2019-12-03 11:44:57 +0100

    add CONFIG+=c++11

--- buildPRAGA.sh
+++ buildPRAGA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd bin/Makeall_PRAGA
-qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug
+qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11
 make -f Makefile qmake_all
 make
