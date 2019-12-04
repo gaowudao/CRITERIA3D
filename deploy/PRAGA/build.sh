8858e3375ba1e68cf499aa0e9e50a3c8d5e76632
lauracosta <lauramail@iol.it>
2019-12-05 00:28:51 +0100

    linuxqtdeploy test

--- deploy/PRAGA/build.sh
+++ deploy/PRAGA/build.sh
@@ -22,10 +22,8 @@ wget -c -nv -O linuxqtdeploy "https://github.com/probonopd/linuxdeployqt/release
 chmod +x linuxqtdeploy
 
 # build appimage
-ls -l bin/PRAGA
-pwd
-ls -l
-find deploy
+ldd bin/PRAGA/PRAGA
+LD_LIBRARY_PATH=`pwd`/mapGraphics/release ldd bin/PRAGA/PRAGA
 
 cp bin/PRAGA/PRAGA deploy/PRAGA/appimage/usr/bin/PRAGA
-./linuxqtdeploy deploy/PRAGA/appimage/usr/share/applications/PRAGA.desktop -qmake=$QMAKE -qmlimport=/opt/qt512/qml -appimage -always-overwrite
+LD_LIBRARY_PATH=`pwd`/mapGraphics/release ./linuxqtdeploy deploy/PRAGA/appimage/usr/share/applications/PRAGA.desktop -qmake=$QMAKE -qmlimport=$QT_DIR/qml -appimage -always-overwrite
