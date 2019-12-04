9ff3caf7c6c8feae3c011aa129d7ebae8bea5a9f
lauracosta <lauramail@iol.it>
2019-12-04 22:10:57 +0100

    test deploy with travis

--- buildPRAGA.sh
+++ buildPRAGA.sh
@@ -8,8 +8,9 @@ make
 cd ..
 
 cd bin/Makeall_PRAGA
-qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler
-make -f Makefile qmake_all
+qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler PREFIX=/usr
+make -f Makefile qmake_all 
 # remove QPalette::PlaceholderText because it is not supported, has been added with Qt 5.12
 # grep -v 'QPalette::PlaceholderText' ../PRAGA/ui_mainWindow.h > ../PRAGA/ui.h; mv -f ../PRAGA/ui.h ../PRAGA/ui_mainWindow.h
-make
+make -j$(nproc)
+make INSTALL_ROOT=appdir -j$(nproc) install ; find appdir/
