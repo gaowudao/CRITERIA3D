84d42002a018091f44a9facc513f051950b7548b
voltAntonio <avolta@arpa.emr.it>
2019-12-11 13:51:10 +0100

    WG2D

--- test/TestWG2D/main.cpp
+++ test/TestWG2D/main.cpp
@@ -78,9 +78,9 @@ int main()
     doy = day = month = year = NODATA;
     prec = minT = maxT = meanT = NODATA;
     bool firstDay = true;
-    int nrStations = 1400; // !! da 1 a 10 stazioni
+    int nrStations = 5; // !! da 1 a 10 stazioni
     int distributionType = 1; // 1 multiexponential 2 multigamma 3 Weibull
-    int yearsOfSimulations = 1; // numero anni
+    int yearsOfSimulations = 1000; // numero anni
     int lengthDataSeries = numberMeteoLines;
     int nrVariables = 3;
     int nrDate = 3;
