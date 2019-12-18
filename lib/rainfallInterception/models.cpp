b662b0b4ba06e2e2e8e68996c38a3e717a5fb85e
voltAntonio <avolta@arpa.emr.it>
2019-12-18 15:59:30 +0100

    WG2D

--- lib/rainfallInterception/models.cpp
+++ lib/rainfallInterception/models.cpp
@@ -85,6 +85,23 @@ namespace canopy {
 
     bool waterManagementCanopy(double* storedWater, double* throughfallWater, double rainfall, double waterFreeEvaporation, double lai, double extinctionCoefficient, double leafStorage, double stemStorage,double maxStemFlowRate, double* freeRainfall, double *drainage, double* stemFlow, double laiMin)
     {
+        // input variables:
+        // double* storedWater: state variable: water stored within the canopy (mm)
+        // double rainfall: hourly rainfall amount (mm)
+        // double waterFreeEvaporation: water evaporated from a free water surface, e.g. a lake) (mm)
+        // double lai leaf area index (m2 m-2) // it must include the cover provided by stems too
+        // input parameters:
+        // double laiMin minimal LAI (m2 m-2) // it must include the cover provided by stems too
+        // double extinctionCoefficient light extinction coefficient of the plant (-)
+        // double leaf storage (mm)
+        // double stemstorge (mm)
+        // double maxStemFlowRate (mm)
+        // output:
+        // double* freeRainfall (mm)
+        // double *drainage (mm)
+        // double* stemFlow (mm)
+        // double* throughfallWater: (mm) water fallen on the ground composed by 2 components: interception free water + intercpted and fallen from leaves and branches (not from the trunk)
+
         double actualCover,actualStorage,grossStorage;
         double interception;
         actualCover = plantCover(lai,extinctionCoefficient,laiMin);
