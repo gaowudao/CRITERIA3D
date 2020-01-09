95ce04922f80af3771694fe1b9b7cc5c19404e71
lauracosta <lauramail@iol.it>
2020-01-09 10:24:23 +0100

    build.sh for new repos

--- deploy/PRAGA/build.sh
+++ deploy/PRAGA/build.sh
@@ -8,11 +8,9 @@ make
 cd -
 
 # build PRAGA
-cd bin/Makeall_PRAGA
-qmake Makeall_PRAGA.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler PREFIX=/usr
+cd makeall
+qmake makeall.pro -spec linux-g++-64 CONFIG+=debug CONFIG+=qml_debug CONFIG+=c++11 CONFIG+=qtquickcompiler PREFIX=/usr
 make -f Makefile qmake_all 
-# remove QPalette::PlaceholderText because it is not supported, has been added with Qt 5.12
-# grep -v 'QPalette::PlaceholderText' ../PRAGA/ui_mainWindow.h > ../PRAGA/ui.h; mv -f ../PRAGA/ui.h ../PRAGA/ui_mainWindow.h
 make 
 
 cd -
@@ -22,5 +20,5 @@ wget -c -nv -O linuxqtdeploy "https://github.com/probonopd/linuxdeployqt/release
 chmod +x linuxqtdeploy
 
 # build appimage
-cp bin/PRAGA/PRAGA deploy/PRAGA/appimage/usr/bin/PRAGA
-LD_LIBRARY_PATH=`pwd`/mapGraphics/release ./linuxqtdeploy deploy/PRAGA/appimage/usr/share/applications/PRAGA.desktop -qmake=$QMAKE -qmlimport=$QT_DIR/qml -appimage -always-overwrite
+cp src/PRAGA deploy/appimage/usr/bin/PRAGA
+LD_LIBRARY_PATH=`pwd`/mapGraphics/release ./linuxqtdeploy deploy/appimage/usr/share/applications/PRAGA.desktop -qmake=$QMAKE -qmlimport=$QT_DIR/qml -appimage -always-overwrite
