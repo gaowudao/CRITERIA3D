f8f1535045df6f73f732f3d8e2de6675b22120e3
voltAntonio <avolta@arpa.emr.it>
2020-01-23 09:47:45 +0100

    WG2D

---
+++ test/TestRainfallInterception/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <stdio.h>
+
+#include "rainfallInterception.h"
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return 0;
+}
