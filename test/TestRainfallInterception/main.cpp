cb84e9a2568c492cfef9f656046a6c29c3498c99
voltAntonio <avolta@arpa.emr.it>
2020-01-28 16:09:01 +0100

    WG2D

--- test/TestRainfallInterception/main.cpp
+++ test/TestRainfallInterception/main.cpp
@@ -24,26 +24,33 @@ int main(int argc, char *argv[])
     int* minute = (int*) calloc(numberOfLines,sizeof(int));
     FILE* fp1;
     fp1 = fopen("weather_Monticolo_all_years_2019_04_21.csv","r");
-    readWeatherMonticoloHalfhourlyData(fp1,minute,hour,dayOfYear,day,month,year,temp, prec);
+    readWeatherMonticoloHalfhourlyData(fp1,minute,hour,dayOfYear,day,month,year,temp, prec, numberOfLines);
     fclose(fp1);
 
-    QCoreApplication a(argc, argv);
+
     double storedWater = 0.0;
-    double rainfall = 1;
-    double waterFreeEvaporation = 0.3;
-    double lai = 4;
-    double lightExtinctionCoefficient = 0.6;
-    double leafStorage = 0.3;
-    double stemStorage = 0.105;
-    double maxStemFlowRate = 0.15;
-    double freeRainfall = 0;
-    double soilWater = 0;
-    double stemFlow = 0;
-    double throughfall = 0;
-    double drainage = 0;
-    canopy::waterManagementCanopy(&storedWater,rainfall,waterFreeEvaporation,lai,0.5,lightExtinctionCoefficient,leafStorage, stemStorage,maxStemFlowRate,&freeRainfall,&drainage,&stemFlow,&throughfall,&soilWater);
-    //canopy::waterManagementCanopy(&storedWater,rainfall,waterFreeEvaporation,lai,0.5,lightExtinctionCoefficient,leafStorage, stemStorage,maxStemFlowRate,&soilWater);
-    printf("soilWater %f\n",soilWater);
-    printf("%f\n",canopy::canopyNoInterceptedRainfallHydrall(lai,0.,rainfall));
+
+    for (int iLine=0;iLine<numberOfLines;iLine++)
+    {
+        //QCoreApplication a(argc, argv);
+
+        double waterFreeEvaporation = 0.01;
+        double lai = 4;
+        double lightExtinctionCoefficient = 0.6;
+        double leafStorage = 0.3;
+        double stemStorage = 0.105;
+        double maxStemFlowRate = 0.15;
+        double freeRainfall = 0;
+        double soilWater = 0;
+        double stemFlow = 0;
+        double throughfall = 0;
+        double drainage = 0;
+        canopy::waterManagementCanopy(&storedWater,prec[iLine],waterFreeEvaporation,lai,0.5,lightExtinctionCoefficient,leafStorage, stemStorage,maxStemFlowRate,&freeRainfall,&drainage,&stemFlow,&throughfall,&soilWater);
+        //canopy::waterManagementCanopy(&storedWater,prec[iLine],waterFreeEvaporation,lai,0.5,lightExtinctionCoefficient,leafStorage, stemStorage,maxStemFlowRate,&soilWater);
+        printf("%.2d:%.2d, %.2d/%.2d/%.4d, %.2f , %.2f\n",hour[iLine],minute[iLine],day[iLine],month[iLine],year[iLine],temp[iLine],prec[iLine]);
+        printf("soilWater %f storedWater%f",soilWater, storedWater);
+        //printf("%f\n",canopy::canopyNoInterceptedRainfallHydrall(lai,0.,rainfall));
+        getchar();
+    }
     return 0;
 }
