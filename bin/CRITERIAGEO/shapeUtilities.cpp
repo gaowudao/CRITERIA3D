a3795979f08ed049c52c07539b0aef271b86b2dd
lauracosta <lauramail@iol.it>
2020-02-04 12:17:18 +0100

    createShapeFromCSV

--- bin/CRITERIAGEO/shapeUtilities.cpp
+++ bin/CRITERIAGEO/shapeUtilities.cpp
@@ -1,5 +1,6 @@
 #include "shapeUtilities.h"
 #include "unitDb.h"
+#include "commonConstants.h"
 
 
 bool extractUCMListToDb(Crit3DShapeHandler* shapeHandler, QString dbName, std::string *error)
@@ -65,6 +66,7 @@ bool createShapeFromCSV(Crit3DShapeHandler* shapeHandler, Crit3DShapeHandler* sh
         *error = "Load shapefile failed: " + ucmShapeFile.toStdString();
         return false;
     }
+    int nShape = shapeFromCSV->getShapeCount();
 
     QFile file(fileCSV);
     if ( !file.open(QFile::ReadOnly | QFile::Text) ) {
@@ -77,24 +79,84 @@ bool createShapeFromCSV(Crit3DShapeHandler* shapeHandler, Crit3DShapeHandler* sh
         // Reads first row
         QString firstRow = in.readLine();
         QStringList newFields = firstRow.split(",");
-        qDebug() << "newFields " << newFields;
-        // LC caratteristiche campi, nomi, tipi?
-        int type = FTString;
-        int nWidth = 40;
-        int nDecimals = 0;
+
+        int type;
+        int nWidth;
+        int nDecimals;
+
+        QMap<int, int> myPosMap;
+
+        int idCaseIndex = shapeFromCSV->getFieldPos("ID_CASE");
+        int idCaseCSV = NODATA;
+
         for (int i = 0; i < newFields.size(); i++)
         {
-            qDebug() << "newFields[i] " << newFields[i];
-            shapeFromCSV->addField(newFields[i].toStdString().c_str(), type, nWidth, nDecimals);
+            if (newFields[i] == "ID_CASE")
+            {
+                idCaseCSV = i;
+            }
+            if (MapCSVShapeFields.contains(newFields[i]))
+            {
+                if (newFields[i] == "CROP")
+                {
+                    type = FTString;
+                    nWidth = 20;
+                    nDecimals = 0;
+                }
+                else
+                {
+                    type = FTDouble;
+                    nWidth = 10;
+                    nDecimals = 2;
+                }
+                shapeFromCSV->addField(MapCSVShapeFields.value(newFields[i]).toStdString().c_str(), type, nWidth, nDecimals);
+                myPosMap.insert(i,shapeFromCSV->getFieldPos(MapCSVShapeFields.value(newFields[i]).toStdString()));
+            }
+
         }
 
+        if (idCaseCSV == NODATA)
+        {
+            *error = "invalid CSV, missing ID_CASE";
+            return false;
+        }
+
+        bool idFound = false;
         //Reads the data up to the end of file
-//        while (!in.atEnd())
-//        {
-//            QString line = in.readLine();
-//            QStringList item = line.split(";");
-//            qDebug() << "item " << item;
-//        }
+        while (!in.atEnd())
+        {
+            QString line = in.readLine();
+            QStringList items = line.split(",");
+            //qDebug() << "items " << items;
+            for (int shapeIndex = 0; shapeIndex < nShape; shapeIndex++)
+            {
+                // check right ID_CASE
+                if (shapeFromCSV->readStringAttribute(shapeIndex, idCaseIndex) == items[idCaseCSV].toStdString())
+                {
+                    idFound = true;
+                    QMapIterator<int, int> i(myPosMap);
+                    while (i.hasNext()) {
+                        i.next();
+                        QString valueToWrite = items[i.key()];
+                        if (shapeFromCSV->getFieldType(i.value()) == FTString)
+                        {
+                            shapeFromCSV->writeStringAttribute(shapeIndex, i.value(), valueToWrite.toStdString().c_str());
+                        }
+                        else
+                        {
+                            shapeFromCSV->writeDoubleAttribute(shapeIndex, i.value(), valueToWrite.toDouble());
+                        }
+
+                    }
+
+                }
+                if (idFound == true)
+                {
+                    idFound = false;
+                    break;
+                }
+            }
+        }
         file.close();
     }
 
--- bin/CRITERIAGEO/shapeUtilities.h
+++ bin/CRITERIAGEO/shapeUtilities.h
@@ -5,6 +5,17 @@
     #include "shapeHandler.h"
 #endif
 #include <QString>
+#include <QMap>
+
+const QMap<QString, QString> MapCSVShapeFields = {
+    { "CROP", "CROPTYPE" },
+    { "readilyAvailableWater", "RAW" },
+    { "rootDepth", "rootDepth" },
+    { "forecast7daysPrec", "FcstPrec7d" },
+    { "forecast7daysMaxTransp", "FcstETc7d" },
+    { "forecast7daysIRR", "FcstIrr7d" },
+    { "previousAllSeasonIRR", "PrevIrrTot" }
+};
 
 bool extractUCMListToDb(Crit3DShapeHandler* shapeHandler, QString dbName, std::string *error);
 bool createShapeFromCSV(Crit3DShapeHandler* shapeHandler, Crit3DShapeHandler* shapeFromCSV, QString fileCSV, std::string *error);
