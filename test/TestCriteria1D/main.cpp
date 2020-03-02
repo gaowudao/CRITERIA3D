b6962de7d2ba370768bf90ff907b94a69eed8682
ftomei <ftomei@arpae.it>
2020-03-02 17:06:22 +0100

    update criteriaModel

--- test/TestCriteria1D/main.cpp
+++ test/TestCriteria1D/main.cpp
@@ -23,8 +23,8 @@ int main(int argc, char *argv[])
         #ifdef TEST
                 QString path;
                 if (! searchDataPath(&path)) return -1;
-                settingsFileName = path + "PROJECT/CLARA/CLARA.ini";
-                //settingsFileName = path + "PROJECT/kiwifruit/kiwifruit.ini";
+                //settingsFileName = path + "PROJECT/CLARA/CLARA.ini";
+                settingsFileName = path + "PROJECT/kiwifruit/kiwifruit.ini";
         #else
                 myProject->logInfo("USAGE: CRITERIA1D settings.ini");
                 return ERROR_SETTINGS_MISSING;
