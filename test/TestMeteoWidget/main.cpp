6d9aa36df66451ab0c694f7c4fbae764d949eeb2
lauracosta <lauramail@iol.it>
2020-04-23 16:12:04 +0200

    read mp db

--- test/TestMeteoWidget/main.cpp
+++ test/TestMeteoWidget/main.cpp
@@ -6,85 +6,57 @@
 #include "console.h"
 
 #include <QRandomGenerator> // test
+#include "dbMeteoPointsHandler.h"
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFileDialog openDBDialog;
+
+    QString dbData = openDBDialog.getOpenFileName(nullptr, ("Open mp database"), "", ("SQLite files (*.db)"));
+
+    if (dbData == "")
+    {
+        return -1;
+    }
+
+    Crit3DMeteoPointsDbHandler mpHandler(dbData);
+    mpHandler.loadVariableProperties();
 
     // test fill meteoPoint with random series
     Crit3DMeteoPoint meteoPoint;
     Crit3DMeteoPoint meteoPointSecond;
-    Crit3DDate firstDate = Crit3DDate(1, 1, 2018);
-    Crit3DDate lastDate = Crit3DDate(31, 12, 2018);
-    float tmin;
-    float tmax;
-    float tavg;
-    float prec;
-    float airRel;
-    meteoPoint.initializeObsDataD(365,firstDate);
-    meteoPoint.setId("1");
-    meteoPointSecond.initializeObsDataD(365,firstDate);
-    meteoPointSecond.setId("2");
-    for (Crit3DDate date = firstDate; date <= lastDate; ++date)
+
+    meteoPoint.setId("10141");
+    meteoPointSecond.setId("10");
+
+    QDateTime first = mpHandler.getFirstDate(daily);
+    QDateTime last = mpHandler.getLastDate(daily);
+    QDate lastDateYear = first.date().addDays(365);
+    if (last.date() < lastDateYear)
     {
-        tmin = QRandomGenerator::global()->generateDouble()*10+2;
-        tmax = QRandomGenerator::global()->generateDouble()*10+5;
-        prec = QRandomGenerator::global()->generateDouble()*10+1;
-        airRel = 5;
-        tavg = (tmin+tmax)/2;
-
-        if (date.month == 1 && date.day == 1)
-        {
-            meteoPoint.setMeteoPointValueD(date, dailyAirTemperatureAvg, 3);
-            meteoPoint.setMeteoPointValueD(date, dailyPrecipitation, 5);
-            meteoPointSecond.setMeteoPointValueD(date, dailyAirTemperatureAvg, 4);
-            meteoPointSecond.setMeteoPointValueD(date, dailyPrecipitation, 6);
-        }
-        else if (date.month == 1 && date.day == 2)
-        {
-            meteoPoint.setMeteoPointValueD(date, dailyAirTemperatureAvg, 5);
-            meteoPointSecond.setMeteoPointValueD(date, dailyAirTemperatureAvg, 7);
-        }
-        else if (date.month == 3 && date.day == 1)
-        {
-            meteoPoint.setMeteoPointValueD(date, dailyAirTemperatureAvg, 7);
-            meteoPoint.setMeteoPointValueD(date, dailyPrecipitation, 15);
-
-            meteoPointSecond.setMeteoPointValueD(date, dailyAirTemperatureAvg, 2);
-            meteoPointSecond.setMeteoPointValueD(date, dailyPrecipitation, 25);
-        }
-        else
-        {
-            meteoPoint.setMeteoPointValueD(date, dailyAirTemperatureMin, tmin);
-            meteoPoint.setMeteoPointValueD(date, dailyAirTemperatureMax, tmax);
-            meteoPoint.setMeteoPointValueD(date, dailyAirTemperatureAvg, tavg);
-            meteoPoint.setMeteoPointValueD(date, dailyPrecipitation, prec);
-            meteoPoint.setMeteoPointValueD(date, dailyAirRelHumidityAvg, airRel);
-
-            meteoPointSecond.setMeteoPointValueD(date, dailyAirTemperatureMin, tmin*0.8);
-            meteoPointSecond.setMeteoPointValueD(date, dailyAirTemperatureMax, tmax*0.8);
-            meteoPointSecond.setMeteoPointValueD(date, dailyAirTemperatureAvg, tavg*0.8);
-            meteoPointSecond.setMeteoPointValueD(date, dailyPrecipitation, prec*0.8);
-        }
-
+        mpHandler.loadDailyData(getCrit3DDate(first.date()), getCrit3DDate(last.date()), &meteoPoint);
+        mpHandler.loadDailyData(getCrit3DDate(first.date()), getCrit3DDate(last.date()), &meteoPointSecond);
     }
-    meteoPoint.initializeObsDataH(1, 15, firstDate);
-    Crit3DTime firstDateTime(firstDate, 0);
-    lastDate = firstDate.addDays(15);
-    Crit3DTime lastDateTime(lastDate, 0);
-    for (Crit3DTime date = firstDateTime; date <= lastDateTime; date=date.addSeconds(3600))
+    else
     {
-        for (int i = 0; i<24; i++)
-        {
-            prec = QRandomGenerator::global()->generateDouble()*10+1;
-            tmin = QRandomGenerator::global()->generateDouble()*10+2;
-            tmax = QRandomGenerator::global()->generateDouble()*10+5;
-            tavg = (tmin+tmax)/2;
+        mpHandler.loadDailyData(getCrit3DDate(first.date()), getCrit3DDate(lastDateYear), &meteoPoint);
+        mpHandler.loadDailyData(getCrit3DDate(first.date()), getCrit3DDate(lastDateYear), &meteoPointSecond);
+    }
 
-            meteoPoint.setMeteoPointValueH(date.date, i, 0, airTemperature, tavg);
-            meteoPoint.setMeteoPointValueH(date.date, i, 0, precipitation, prec);
-        }
+    first = mpHandler.getFirstDate(hourly);
+    last = mpHandler.getLastDate(hourly);
+    lastDateYear = first.date().addDays(10);
+    if (last.date() < lastDateYear)
+    {
+        mpHandler.loadHourlyData(getCrit3DDate(first.date()), getCrit3DDate(last.date()), &meteoPoint);
+        mpHandler.loadHourlyData(getCrit3DDate(first.date()), getCrit3DDate(last.date()), &meteoPointSecond);
+    }
+    else
+    {
+        mpHandler.loadHourlyData(getCrit3DDate(first.date()), getCrit3DDate(lastDateYear), &meteoPoint);
+        mpHandler.loadHourlyData(getCrit3DDate(first.date()), getCrit3DDate(lastDateYear), &meteoPointSecond);
     }
 
     #ifdef _WIN32
@@ -100,8 +72,8 @@ int main(int argc, char *argv[])
     std::cout << "...draw data p1\n" << std::flush;
     w.draw(meteoPoint);
 
-    //std::cout << "...draw data p2\n" << std::flush;
-    //w.draw(meteoPointSecond);
+    std::cout << "...draw data p2\n" << std::flush;
+    w.draw(meteoPointSecond);
 
     return a.exec();
 }
