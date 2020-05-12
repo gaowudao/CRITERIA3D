c703024d38467dcc974c87c7057f1a24f5184f6a
lauracosta <lauramail@iol.it>
2020-05-12 13:43:12 +0200

    add projectPath meteoWidget

--- test/TestMeteoWidget/main.cpp
+++ test/TestMeteoWidget/main.cpp
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
     #endif
 
     std::cout << "\n...Create window\n" << std::flush;
-    Crit3DMeteoWidget w("Point");
+    Crit3DMeteoWidget w("Point", "/home/laura/PRAGA/DATA/PROJECT/test/");
 
     std::cout << "...Show window\n" << std::flush;
     w.show();
